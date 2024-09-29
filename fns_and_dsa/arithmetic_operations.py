d424a66b08980b1d5534b678ba1708d6855c21a9
Fatima Ezzahraa Elouidad <titimaelouidad@gmail.com>
2024-09-29 09:33:03 +0100

    added fns_and_dsa.py file

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,17 @@
+def perform_operation(num1, num2, operation):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Error: Division by zero is not allowed"
+        elif num2 != 0:
+            return num1 / num2
+    else:
+        'Invalid operation'
+
+
+        
