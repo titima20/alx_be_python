3e0d109d12eef11fa63ec0a5e994fc921ae11eca
Fatima Ezzahraa Elouidad <titimaelouidad@gmail.com>
2024-10-12 16:49:32 +0100

    done

--- oop/library_system.py
+++ oop/library_system.py
@@ -2,14 +2,21 @@ class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
+
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
 class EBook(Book):
       def __init__(self, title, author, file_size):
         Book.__init__(self, title, author)
         self.file_size = file_size
+      def __str__(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         Book.__init__(self, title, author)
         self.page_count = page_count
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 class Library:
     def __init__(self):
         self.books = []
